bae488297b2c50b4710e5e9bebbe9260e06384be
NeilMcNeil <119974057+NeilMcNeil@users.noreply.github.com>
2022-12-06 15:49:56 -0500

    Update MyRobot.java

--- myRobot/src/main/java/frc/robot/MyRobot.java
+++ myRobot/src/main/java/frc/robot/MyRobot.java
@@ -6,36 +6,3 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
-@SuppressWarnings("all")
-public class MyRobot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {}
-
-  /** This function is run once each time the robot enters autonomous mode */
-  @Override
-  public void autonomousInit() {}
-
-  /** This function is called periodically during autonomous */
-  @Override
-  public void autonomousPeriodic() {}
-
-  /** This function is called once each time the robot enters tele-operated mode */
-  @Override
-  public void teleopInit() {}
-
-  /** This function is called periodically during operator control */
-  @Override
-  public void teleopPeriodic() {}
-
-  /** This function is called periodically during test mode */
-  @Override
-  public void testPeriodic() {}
-
-  /** This function is called periodically during all modes */
-  @Override
-  public void robotPeriodic() {}
-}
