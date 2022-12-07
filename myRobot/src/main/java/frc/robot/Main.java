f1e1b0e4be95622342d6243f67f26c0a987e00ff
grayw-tcdsb <119452922+grayw-tcdsb@users.noreply.github.com>
2022-12-07 15:18:52 -0500

    Update Main.java

--- myRobot/src/main/java/frc/robot/Main.java
+++ myRobot/src/main/java/frc/robot/Main.java
@@ -1,6 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
+// hey John!!
 
 package frc.robot;
 
@@ -14,6 +15,7 @@ public final class Main {
    *
    * <p>If you change your main robot class, change the parameter type.
    */
+  
   public static void main(String... args) {
     RobotBase.startRobot(MyRobot::new);
   }
